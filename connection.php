49bc7147d71e1f3fe4d8e9f91898d14d6f42a6fa
Lahiru Buddhika <96839781+lahiru2001@users.noreply.github.com>
2023-10-07 16:16:03 +0530

    Crud Operation Using oop

--- connection.php
+++ connection.php
@@ -1,6 +1,38 @@
-//Make the connection with database
 <?php
 
- $db=mysqli_connect("localhost","root","","ida");
+class database{
+    private $hostname ="localhost";
+    private $username ="root";
+    private $password ="";
+    private $dbname ="ida";
+    public $conn;
+
+
+
+    function __construct()
+    {
+        $this->conn = new mysqli($this->hostname,$this->username,
+        $this->password,$this->dbname);
+
+        if($this->conn->connect_error)
+        {
+            echo "connection Error";
+        }else
+        {
+            echo "connection sucess";
+        }
+
+    }
+}
+
+$obj = new database;
+
+   
+
+
+
+
+
+
 
 ?>
